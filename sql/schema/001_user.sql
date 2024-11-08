7470cf87f26a1c9657ff413a510743b5ceb981f9
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-11-08 15:17:58 +0100

    Add chirps table to goose migrations

--- sql/schema/001_user.sql
+++ sql/schema/001_user.sql
@@ -6,6 +6,15 @@ CREATE TABLE users(
 	email TEXT NOT NULL UNIQUE
 
 );
+CREATE TABLE chirps(
+	id UUID PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	body TEXT NOT NULL,
+	user_id UUID,
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 -- +goose Down
+DROP TABLE chirps;
 DROP TABLE users;

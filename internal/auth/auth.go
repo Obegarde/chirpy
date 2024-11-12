700d5db250bb61d0ea9dc44e87912d6c73b1d6d6
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-11-12 21:08:02 +0100

    Add first half of the token implementation

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,9 @@
 package auth
 import(	
 	"golang.org/x/crypto/bcrypt"
-	
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string)(string, error){
@@ -15,4 +17,17 @@ func HashPassword(password string)(string, error){
 //Returns nil on success and an error on fail
 func CheckPasswordHash(password string, hash string) error{
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error){
+	claimsStruct := jwt.RegisteredClaims{
+		Issuer:"chirpy",
+		IssuedAt:jwt.NewNumericDate(time.Now()),
+		ExpiresAt:jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:userID.String(),
+	}
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256,claimsStruct)
+	return newToken.SignedString(tokenSecret)
+
 }
